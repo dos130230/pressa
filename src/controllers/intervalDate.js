2b97c4c43de904f7338a3c1e4fb293c5c991380a
Dostonbek O'ktamov <88940496+dos130230@users.noreply.github.com>
2022-02-03 18:12:32 +0500

    bag fixed

--- src/controllers/intervalDate.js
+++ src/controllers/intervalDate.js
@@ -12,10 +12,10 @@ const GET = async (req,res,next)=> {
 			),
 			post as (
 			DELETE FROM posters
-			WHERE post_id=ANY(select post_id from my_del_post))
+			WHERE post_id= ANY(select post_id from my_del_post))
 
 			DELETE FROM users
-			WHERE user_id=ANY(select post_id from my_del_post)
+			WHERE user_id = ANY(select post_id from my_del_post)
 			;
 			`
 			)
